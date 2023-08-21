35d4c93db6ab69b5e7430a11fa8a440012b51f14
DraganovTs <cvetomir_draganov@abv.bg>
2023-08-21 20:08:50 +0300

    init

---
+++ question-service/src/test/java/com/homecode/questionservice/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homecode.questionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
