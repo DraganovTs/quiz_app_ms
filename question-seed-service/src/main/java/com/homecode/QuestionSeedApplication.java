d3a4358dd2e4d3a7797cff261822c16e60d3fc23
DraganovTs <cvetomir_draganov@abv.bg>
2023-09-03 09:50:52 +0300

    question seed service

---
+++ question-seed-service/src/main/java/com/homecode/QuestionSeedApplication.java
@@ -0,0 +1,11 @@
+package com.homecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionSeedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionSeedApplication.class);
+    }
+}
